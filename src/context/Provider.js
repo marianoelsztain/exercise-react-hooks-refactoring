9fb830003c49528998dab176b147bf645a0f7f80
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-11-16 16:04:25 -0300

    changed provider do function

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,63 +1,50 @@
 // src/context/Provider.js
 
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-      signal: {
-        color: 'red',
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-    this.changeSignal = this.changeSignal.bind(this);
-  }
+function Provider({ children }) {
+  const initialCarsState = {
+    red: false,
+    blue: false,
+    yellow: false,
+  };
+  const initialSignalState = {
+    color: 'red',
+  };
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
+  const [ cars, setCars ] = useState(initialCarsState);
+  const [ signal, setSignal ] = useState(initialSignalState);
+
+  const moveCar = (car, side) => {
+    setCars({
+        ...cars,
         [car]: side,
       },
-    });
+    );
   };
 
-  changeSignal(signalColor) {
-    this.setState({
-      signal: {
-        ...this.state.signal,
+  const changeSignal = (signalColor) => {
+    setSignal({
+        ...signal,
         color: signalColor,
       },
-    });
+    );
   };
 
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-      changeSignal: this.changeSignal,
-    };
 
-    const { children } = this.props;
+  const context = {
+    cars,
+    signal,
+    moveCar,
+    changeSignal,
+  };
 
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
-};
-
-Provider.propTypes = {
-  children: PropTypes.node.isRequired,
+  return (
+    <CarsContext.Provider value={context}>
+      {children}
+    </CarsContext.Provider>
+  );
 };
 
 export default Provider;
